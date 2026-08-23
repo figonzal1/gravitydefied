573f9c347294f9ae43ea158fbd9952545337cf7a
figonzal1 <figonzal.dev@gmail.com>
2026-08-23 12:22:57 -0400

    build: bump version to 1.0.8 (9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "cl.figonzal.gravitydefied"
         minSdk = 23
         targetSdk = 37
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
     }
 
     signingConfigs {
